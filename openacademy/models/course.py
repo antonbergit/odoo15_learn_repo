5b908307efc45c6898e82cf7aedaf8300e9f8139
antonbergit <anton.berg.it@gmail.com>
2022-05-18 11:49:16 +0300

    adding openacademy module

---
+++ openacademy/models/course.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class course(models.Model):
+    _name = 'course'
+    _description = 'test odoo//python class'
+
+    title = fields.Char(string="Title", required=True)
+    description = fields.Text(string="Description", required=False)
