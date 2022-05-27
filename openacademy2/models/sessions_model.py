26d6120dfe1b4d9e5bcf13378002e478b45e3a04
antonbergit <anton.berg.it@gmail.com>
2022-05-27 13:29:27 +0300

    default values

--- openacademy2/models/sessions_model.py
+++ openacademy2/models/sessions_model.py
@@ -6,10 +6,20 @@ class sessions(models.Model):
     _description = 'Quest 2.3.7'
 
     name = fields.Char(string = "Session ident name")
-    start_date = fields.Date(string = "Firs day")
+    start_date = fields.Date(string = "First day", default=Date.today())
     duration = fields.Integer(string = "Duration on weeks")
     seats = fields.Integer(string = "Number of seats")
 
     instructor = fields.Many2one('res.partner', string = "Instructor/Trainer")
     course = fields.Many2one('openacademy2.courses', required = False, string = "Course id")
     attendees = fields.Many2many('res.partner', string = "Attendees")
+
+    seats_reserved = fields.Float(compute = '_seats_reserved')
+
+    @api.depends('seats','attendees')
+    def _seats_reserved(self):
+        for rec_session in self:
+            if (rec_session.seats != 0):
+                rec_session.seats_reserved = 100 * len(rec_session.attendees) / rec_session.seats
+            else:
+                rec_session.seats_reserved = 0.0
