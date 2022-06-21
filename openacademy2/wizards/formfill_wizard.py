1afdd4a7690e4cbd5e13922d567f076d38f2164e
antonbergit <anton.berg.it@gmail.com>
2022-06-21 17:10:42 +0300

    Quest stage 2.3.16.1+2+3 completed

--- openacademy2/wizards/formfill_wizard.py
+++ openacademy2/wizards/formfill_wizard.py
@@ -13,15 +13,12 @@ class formfill(models.TransientModel):
         if (temp_session):
             return temp_session.attendees
 
-    rel_sessions = fields.Many2one('openacademy2.sessions', string = "Session", default=default_rel_sessions)
+    rel_sessions = fields.Many2many('openacademy2.sessions', string = "Session", default=default_rel_sessions)
     rel_partner = fields.Many2many('res.partner', string = "Attendees", default=default_rel_partner)
 
     def save_attendees(self):
-        temp_own_session = self.rel_sessions
-        temp_far_session = self.env['openacademy2.sessions'].browse(self._context.get('active_ids'))
-
-        temp_far_session.attendees = False
-
-        if (temp_own_session):
-            for temp_attendee in self.rel_partner:
-                temp_far_session.attendees |= temp_attendee
+        for temp_session in self.rel_sessions:
+            if (temp_session):
+                temp_session.attendees = False
+                for temp_attendee in self.rel_partner:
+                    temp_session.attendees |= temp_attendee
